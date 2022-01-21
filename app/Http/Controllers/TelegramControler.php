bba1d11c1d5bebe0ab114cd790c1c5e7dcfa3051
Sanya Coder <clashoffclans210@gmail.com>
2022-01-21 18:12:40 +0200

    test 4

--- app/Http/Controllers/TelegramControler.php
+++ app/Http/Controllers/TelegramControler.php
@@ -29,12 +29,12 @@ class TelegramControler extends Controller
      *
      *
      */
-    public function update()
+    public function update(Request $request)
     {
 
 
         $telegram = new Api("5052566047:AAHiqDUingQ8UmjqlRgAbyAsg-V4Trzxqow");
-        dd($telegram->getWebhookUpdate());
+        dd($request->all());
         $telegram->addCommand(StartCommand::class);
         $telegram->addCommand(GetContactCommand::class);
         $telegram->addCommand(CityCommand::class);
